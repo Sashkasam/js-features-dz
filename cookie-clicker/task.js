cf2947a121baed0b84ae365481617b863f3710f7
Alexsander <sparco@nxt.ru>
2024-03-10 16:17:03 +0500

    some correct

--- cookie-clicker/task.js
+++ cookie-clicker/task.js
@@ -1,17 +1,22 @@
 const clickerCounter = document.getElementById('clicker__counter')
 const cookie = document.getElementById('cookie')
 const clickerSpeed = document.getElementById('speed__counter')
-let flag = false;
-cookie.onclick = () => { 
+// let flag = false;
+// cookie.onclick = () => { 
     
-    flag = !flag
-    clickerCounter.textContent = Number(clickerCounter.textContent) + 1;
-    if(flag) {
-        cookie.width += 20;
-        cookie.height += 20;
-    }else {
-        cookie.width -= 20;
-        cookie.height -= 20;
-    }   
- }
+//     flag = !flag
+//     clickerCounter.textContent = Number(clickerCounter.textContent) + 1;
+//     if(flag) {
+//         cookie.width += 20;
+//         cookie.height += 20;
+//     }else {
+//         cookie.width -= 20;
+//         cookie.height -= 20;
+//     }   
+//  }
+
+//  Более простой вариант 
+cookie.addEventListener('click', function(){
+    cookie.width = ++ clickerCounter.textContent % 2 ? 250 : 200
+})
 
--- mole-game/base.js
+++ mole-game/base.js
@@ -12,13 +12,24 @@ for (let hole of allHoles) {
         
         if (deadCount.textContent == 10) {
             alert('Победа!')
-            deadCount.textContent = 0
-            lostCount.textContent = 0
+            resetScore(alert)
+            // deadCount.textContent = 0
+            // lostCount.textContent = 0
         }else if(lostCount.textContent == 5) {
             alert('Вы проиграли!')
-            deadCount.textContent = 0
-            lostCount.textContent = 0
+            resetScore(alert)
+        //     deadCount.textContent = 0
+        //     lostCount.textContent = 0
         }
 
+    }
+}
+
+//  C помощью функции
+
+function resetScore (alert) {
+    if (alert) {
+        deadCount.textContent = 0
+        lostCount.textContent = 0
     }
 }
